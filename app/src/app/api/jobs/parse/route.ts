07c060ee46a78284268c9f304aa736df9255b541
Mael-Chao <maelchao@gmail.com>
2026-05-24 06:31:55 -0500

    fix gemini version

--- app/src/app/api/jobs/parse/route.ts
+++ app/src/app/api/jobs/parse/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 const GEMINI_API_KEY = "AIzaSyD6aEfDxvgouRqZ3l1zfpJwjaOamjQI1gc" //process.env.GEMINI_API_KEY;
-const GEMINI_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`;
+const GEMINI_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro:generateContent?key=${GEMINI_API_KEY}`;
 
 export async function POST(request: Request) {
   const { text } = await request.json();
